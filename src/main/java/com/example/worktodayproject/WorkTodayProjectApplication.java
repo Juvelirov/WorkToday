b24049bc84cbcd8fa4b3332fc301014e9dfda1d5
alivkas <mr1504dj@mail.ru>
2024-10-26 12:04:25 +0500

    init

---
+++ src/main/java/com/example/worktodayproject/WorkTodayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.worktodayproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTodayProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkTodayProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/worktodayproject/WorkTodayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worktodayproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTodayProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
