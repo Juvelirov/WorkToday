cd040d632cdb87aeccd94f69e917d000f4f15627
matvey.e <matvey.e@mail.ru>
2024-10-27 13:40:47 +0500

    create user and role tables

---
+++ src/main/java/com/example/worktodayproject/database/entity/Users.java
@@ -0,0 +1,53 @@
+package com.example.worktodayproject.database.entity;
+
+import com.example.worktodayproject.database.entity.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users", schema = "public")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+public class Users extends BaseEntity implements UserDetails {
+
+    @Column(name = "login")
+    String login;
+    @Column(name = "passwsord")
+    String password;
+    @Column(name = "created_at")
+    Date create;
+    @Column(name = "email")
+    String email;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = {
+                    @JoinColumn(name = "user_id")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "role_id")
+            }
+    )
+    Set<Roles> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+}
