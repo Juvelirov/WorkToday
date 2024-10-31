4b11137a41352528267de0b8a3714352bda824f7
Gas <omarov.ggasan@gmail.com>
2024-10-31 15:01:36 +0500

    set up schadcn ui

--- src/main/front/src/App.tsx
+++ src/main/front/src/App.tsx
@@ -1,7 +1,8 @@
+import { RegistrationForm } from "./components/RegistrationForm/RegistrationForm";
 import "./App.css";
 
 function App() {
-  return <>Huh</>;
+  return <RegistrationForm />;
 }
 
 export default App;
---
+++ src/main/front/src/types.ts
@@ -0,0 +1,12 @@
+export type s = string;
+export type n = number;
+
+export type ss = string[];
+export type nn = number[];
+
+// biome-ignore lint/suspicious/noConfusingVoidType: <explanation>
+export type v = void;
+
+//
+
+export type DivRef = React.RefObject<HTMLDivElement>;
